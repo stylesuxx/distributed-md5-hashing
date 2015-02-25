3714a40120bd8db827453d151c39e4416930c5d9
stylesuxx <stylesuxx@gmail.com>
2015-02-25 21:15:50 +0100

    Disabled put, patch and deltete on all api endpoints

--- server/api/search/index.js
+++ server/api/search/index.js
@@ -9,8 +9,8 @@ var router = express.Router();
 router.get('/', controller.index);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
-router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
+router.put('/:id', auth.hasRole('admin'), controller.update);
+router.patch('/:id', auth.hasRole('admin'), controller.update);
 router.delete('/:id', auth.hasRole('admin'), controller.destroy);
 
 module.exports = router;
